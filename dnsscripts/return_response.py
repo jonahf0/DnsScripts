8cecead952af59df20ca8c6848be3862c8dcf73d
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-03-02 14:08:34 -0600

    modified query_data to take a specific type of query, such as A, NS, SOA, etc.

--- dnsscripts/return_response.py
+++ dnsscripts/return_response.py
@@ -1,16 +1,16 @@
 from dns import resolver, query, message
 
-def query_data(name, server):
+def query_data(name, server, query_type):
 
     serv_addr = resolver.resolve(server, "A")[0].address
 
-    name_message = message.make_query(name, "A")
+    name_message = message.make_query(name, query_type)
 
     return name_message, serv_addr
 
 def return_response(name, server):
     
-    name_message, server_addr = query_data(name, server)
+    name_message, server_addr = query_data(name, server, "A")
 
     response = query.udp(name_message, server_addr, 2.0)
 
@@ -18,7 +18,7 @@ def return_response(name, server):
 
 def return_response_tcp(name, server):
 
-    name_message, server_addr = query_data(name, server)
+    name_message, server_addr = query_data(name, server, "A")
 
     response = query.tcp(name_message, server_addr, 2.0)
 
@@ -27,7 +27,7 @@ def return_response_tcp(name, server):
 
 def return_response_norecurse(name, server):
     
-    name_message, server_addr = query_data(name, server)
+    name_message, server_addr = query_data(name, server, "A")
 
     name_message.flags = 0
     
