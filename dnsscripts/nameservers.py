c7377af2f2d9b2c71f34b79bc2608617da478cd1
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-03-02 14:11:25 -0600

    fixed a bug where get_master_ns where there is a NoAnswer error; added an except block that queries using query and query_data functions

--- dnsscripts/nameservers.py
+++ dnsscripts/nameservers.py
@@ -1,19 +1,34 @@
-from dns import resolver
+from dns import resolver, query
+from return_response import query_data
+from reverse_lookup import reverse_lookup
 
-def get_master_ns(name):
-
-    response = resolver.resolve(name, "SOA")
-
-    return response.response.answer[0][0].mname.to_text()
-
+#tries to get a list of nameservers from a host
 def get_ns(name):
 
     nameservers = []
 
+    #gets nameservers response message
     response = resolver.resolve(name, "NS").response.answer[0]
 
-    for ns in response:
+    return [ ns.target.to_text() for ns in response ]
 
-        nameservers.append( ns.target.to_text() )
+#try to get the master nameserver for a domain;
+#can give an optional nameserver for resolution in case the master does
+#not want to cooperate and give an SOA, or can let the default for that nameserver
+#be from the default resolver's list
+def get_master_ns(name, server=resolver.get_default_resolver().nameservers[0]):
 
-    return nameservers
+    try:
+        response = resolver.resolve(name, "SOA")
+
+        return response.response.answer[0][0].mname.to_text()
+
+    except resolver.NoAnswer:
+
+        server_name = reverse_lookup(server)
+
+        message, address = query_data(name, server_name, "SOA")
+
+        response = query.udp(message, address)
+
+        return response.authority[0].to_rdataset()[0].mname.to_text()
