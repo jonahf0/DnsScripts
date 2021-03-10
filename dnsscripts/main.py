f06420ab533037700364c0af9506de1972cc5c64
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-03-10 09:28:59 -0600

    began modifiying main; added argparser and ns functions

--- dnsscripts/main.py
+++ dnsscripts/main.py
@@ -1,5 +1,22 @@
-def main():
-    pass
+from argparse import ArgumentParser
+from nameservers import get_ns
+
+def main(domain):
+
+    ns = get_ns(domain)
+
 
 if __name__ == "__main__":
-    pass
+    
+    parser = ArgumentParser()
+
+    parser.add_argument("-domain", help="the domain to get information for")
+
+    #add:
+    #   cache snooping 
+    #   discovery methods
+    #
+
+    args = parser.parse_args()
+    
+    main(args.domain)
