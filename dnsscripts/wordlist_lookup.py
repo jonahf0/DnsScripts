7baa06357a2c627d0363e0925b70712511bb22b8
jonahf0 <jonahfitzgerald0113@gmail.com>
2021-03-12 08:54:15 -0600

    finished the main lookup functionality, where there is a try to resolve for each word

--- dnsscripts/wordlist_lookup.py
+++ dnsscripts/wordlist_lookup.py
@@ -1,74 +1,89 @@
 from argparse import ArgumentParser
-from return_response import return_response
-from host_discovery import reverse_forward_lookup
-from dns import rcode
+from dns import resolver
+from socket import gethostbyname
+from itertools import repeat
 from ipaddress import ip_address
 import threading as th
 
-def range_lookup(ip, server, space=5):
+#returns a function based on the resolver created by passing a
+#specific server to the "creater" function
+def resolve_with_server_closure(server=None):
 
-    middle_address = ip_address(ip)
+    #create the resolver, give it the server to use,
+    #then "bind" that resolver to a function to return that resolves
+    #hostnames
+    res = resolver.Resolver()
 
-    for num in range(0, space):
+    if server != None:
+        res.nameservers = [server]
 
-        response = reverse_forward_lookup( middle_address - space + num, server)
+    def func_to_return(hostname, out=False):
 
-        analyze_response( response, server)
+        try:
 
-    for num in range(1, space + 1):
+            response = res.resolve(hostname, "A")
 
-        response = reverse_forward_lookup( middle_address + num, server )
+            ip = response.rrset[0].to_text()
 
-        analyze_response( response, server )
+            if out:
 
-def analyze_response(response, server, space=5):
+                print("{}: {}".format(hostname, ip))
+
+            return ip
+
+        except Exception as e:
+
+            return False
+
+    return func_to_return
+
+#creates a list from a text "wordslist" file, then decides which function
+#to map to the list based on whether there is a server or not;
+#instead of using the resolver closure for both cases, resolve_hostname
+#is used because of speed improvements
+def wordlist_lookup(domain, wordlist, server=None, threads=0, out=False):
     
-    rc = response.resolve_chaining()
-
-    if rc.cnames != []:
-
-        cname_pointing = " --> ".join([ x.to_rdataset()[0].to_text() for x in rc.cnames ])
-
-        addresses = [ ip.to_text() for ip in rc.answer ]
-
-        for ip in addresses:
-            range_lookup(ip, server, space=5)
-
-        return "".join( [cname_pointing, "".join(addresses) ] )
-
-    elif rc.answer != None:
-        
-        address = rc.answer[0].to_text()
-
-        return address
-
-def wordlist_lookup(domain, server, wordlist, space=5):
-
     with open(wordlist, "r") as f:
+
+        #in the list comprehension, remove the newline characters
         words = [ word.replace("\n", "") for word in f ]
 
-    for word in words:
+    #call the closure function to return a resolver function;
+    #if server == None, then the resolver that is bound by the closure
+    #simply uses the host's nameservers for resolving
+    resolver_func = resolve_with_server_closure(server)
+    
+    #since the resolver function returns False if there is no resolution,
+    #filter removes False entries
+    ips = list(
+                filter(
+        
+                    lambda b: b != False,
+                    
+                    map(
+                        resolver_func,
 
-        response = return_response(
-            word + "." + domain, server
+                        [ word+"."+domain for word in words ],
+
+                        repeat(out)
+                    )
+            )
         )
+         
+    #ips = [ ip for ip in [ resolver_func(word+"."+domain, out) for word in words ] if ip != False ]
 
-        if rcode.to_text(response.rcode()) != "NXDOMAIN":
-            
-            address_string = analyze_response(response, server, space)
-            
-            print("{} --> {}".format(word+"."+domain, address_string))
+    return ips
 
 if __name__ == "__main__":
 
     parser = ArgumentParser()
 
-    parser.add_argument("domain")
-    parser.add_argument("server")
-    parser.add_argument("wordlist")
-    parser.add_argument("--space")
-    parser.add_argument("--threads", type=int,default=0)
+    parser.add_argument("domain", help="domain to analyze and perform reverse lookups for")
+    parser.add_argument("wordlist", help="a list of words to create test hostnames to bruteforce lookup; hopefully some of the words will reveal actual hosts")
+    parser.add_argument("--server", help="optional server to use to lookup, with the default being the user's nameservers; a good place to try would be the target domain's servers")
+    parser.add_argument("--space", help="how many addresses above and below a successful wordlist-lookup host the program should try to reverse lookup", type=int, default=5)
+    parser.add_argument("--threads", help="how many threads should be used for performing the bruteforcing", type=int,default=0)
 
     args = parser.parse_args()
 
-    wordlist_lookup(args.domain, args.server, args.wordlist, args.threads)
+    wordlist_lookup(args.domain, args.wordlist, args.server, args.threads, True)
